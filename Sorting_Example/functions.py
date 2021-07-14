5f223141d2fe9d6151e80d04a66fc222b03e6e05
Ziqi Wang <wangziqi000@126.com>
2021-07-14 00:19:30 -0700

    project colab

---
+++ Sorting_Example/functions.py
@@ -0,0 +1,20 @@
+#Functions for sorting exercise
+
+def cmp(a, b):
+    return (a > b) - (a < b) 
+
+def mySort(numbers):
+    numbers = bubbleSort(numbers)
+    return numbers
+
+def bubbleSort(nums):
+    """
+    :type nums: List[int]
+    :rtype: List[int]
+    """
+    #################################
+    
+    # Write your code here
+    
+    #################################
+    return nums
