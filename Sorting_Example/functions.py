05551e4d509a5a8eddf9c370faac01b1abb710d7
Ziqi Wang <wangziqi000@126.com>
2021-07-14 18:33:56 -0700

    M2 fix

--- Sorting_Example/functions.py
+++ Sorting_Example/functions.py
@@ -13,6 +13,34 @@ def bubbleSort(nums):
     :rtype: List[int]
     """
     #################################
+    # Selection Sort
+    n = len(nums)
+    if n<=1:
+        return nums
+    for i in range(n):
+        smallest_val = nums[i]
+        smallest_idx = i
+        # find the smallest element
+        for j in range(i,n):
+            temp = nums[j]
+            if temp <= smallest_val:
+                smallest_idx = j
+                smallest_val = temp
+            # print(smallest_val)
+        # swap it with nums[i]
+        temp = nums[i]
+        nums[i] = nums[smallest_idx]
+        nums[smallest_idx] = temp
+        
+    return nums
+
+        
+        
+        
+     
+        
+        
+    
     
     # Write your code here
     
